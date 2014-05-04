92707e305844fdc08688089e925263bbecbc0b09
Justin McNamara <jmcnamara7489@gmail.com>
2014-05-03 21:44:02 -0400

    Changes

--- src/LargestPrimeFactor.java
+++ src/LargestPrimeFactor.java
@@ -2,25 +2,26 @@
 import java.math.BigInteger;
 
 public class LargestPrimeFactor {
-     static BigInteger numm;
-    public static void main(String[] args){
-      numm = new BigInteger("600851475143");
-      BigInteger newnumm = numm;
-      BigInteger largestFact = new BigInteger("0");
-      
-     // BigInteger sumNum = num1.add(num2);
-       // System.out.println(sumNum); 
-    
+
+    static BigInteger numm;
+
+    public static void main(String[] args) {
+        numm = new BigInteger("600851475143");
+        BigInteger newnumm = numm;
+        BigInteger largestFact = new BigInteger("0");
+
     }
-    
-    boolean isPrime(int n){
-        if(n%2 == 0) return false;
-        
-        for(int i=3; i*i<=n;i++){
-            if(n%i==0)
+
+    boolean isPrime(int n) {
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= n; i++) {
+            if (n % i == 0) {
                 return false;
+            }
         }
         return true;
     }
-    
 }
